c93750dbd586419dd757e88944816d458cca92fb
lin <529029157@qq.com>
2019-07-28 16:12:57 +0800

    springboot  helloworld

--- springboot_helloworld/src/main/java/com/lin/springboot/controller/HelloController.java
+++ springboot_helloworld/src/main/java/com/lin/springboot/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.lin.springboot.controller;
 
+import com.lin.springboot.pojo.Hello;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(){
+    @PostMapping("/hello")
+    public String hello(@RequestBody Hello hello){
+        System.out.println(hello.getMsg());
+        return  "hello";
+    }
+    @PostMapping("/hi")
+    public String hi(Hello hello){
+        System.out.println(hello.getMsg());
         return  "hello";
     }
 }
