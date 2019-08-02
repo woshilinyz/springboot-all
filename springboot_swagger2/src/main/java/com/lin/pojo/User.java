d0a7898f951ac8abda511edac15f549fbe923517
lin <529029157@qq.com>
2019-08-02 15:34:35 +0800

    springboot 整合 swagger2

--- springboot_swagger2/src/main/java/com/lin/pojo/User.java
+++ springboot_swagger2/src/main/java/com/lin/pojo/User.java
@@ -1,5 +1,7 @@
 package com.lin.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -7,8 +9,13 @@ import lombok.Data;
  * @create: 2019-08-02 15:27:50
  **/
 @Data
+@ApiModel
 public class User {
 
+    @ApiModelProperty("用户id")
     private Long id;
+    @ApiModelProperty("用户名")
     private String username;
+    @ApiModelProperty("用户地址")
+    private String address;
 }
